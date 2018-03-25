ad4c0baab489c1a11b22b8a1c15e976ee8b1763c
samu101108 <samu101108@gmail.com>
2018-03-25 15:46:41 -0300

    finalizada a lógica para imagens de fundo de acordo com o tempo atual, adicionado estilos e imagens.

--- Local Weather/js/app.js
+++ Local Weather/js/app.js
@@ -4,50 +4,96 @@
       var latitude = position.coords.latitude; //assign latitude to a variable
       var longitude = position.coords.longitude; // assign longitude to a variable
       var unit = ["&units=metric", "&units=imperial"];
-      // o toggle da metrica é igual ao projeto da lampada click (esta no codepen)
+      // unit = unit[1];
+      // if(unit==1){
+      //   unit = unit[1];
+      // }
+          // fazer um assincrono com
+          // xmlHttpRequest
+          // para mudar só a unidade de medida e mostrar na tela
+          // só o 'ºF'
+          $("#unit").click(function() {
+            $.get("https://api.openweathermap.org/data/2.5/weather?lat=-8.1199301&lon=-34.945139&units=metric&APPID=9c2f8e61e7b4c8d97a651fe5a5b04868", function(data, status) {
+              alert ("data: " + data + "\nStatus: " + status);
+            });
+          });
+
+
       var token = "&APPID=9c2f8e61e7b4c8d97a651fe5a5b04868";
-
       $.getJSON(url + "lat=" + latitude + "&lon=" + longitude + unit[0] + token, function(a) {
-        // ifs para as imagens
-
-        // if(day){
-          // background-color: lightblue;
-        //   if(clouds) {}
-        //   if(rain) {}
-        //   if(snow) {}
-        //   if(hail) {}
-        // }
-        // elseif(night){
-          // background-color: navy;
-        //   if(clouds) {}
-        //   if(rain) {}
-        //   if(snow) {}
-        //   if(hail) {}
-        // }
-        // else{ console.log("Tempo não determidado");}
-
 
         var temperature = Math.round(a.main.temp);
 
         $('#temperature').html(temperature);
         $('#city').html(a.name);
-        $('#main').html(a.weather[0].main);
+        $('#main').html(a.weather[0].description);
 
         // icone
         var iconcode = a.weather[0].icon;
         var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
         $('#wicon').attr('src', iconurl);
 
-        $('#max').html(a.main.temp_max);
-        $('#min').html(a.main.temp_min);
-        $('#description').html(a.weather[0].description);
-
-        // $('#country').html(a.sys.country);
-        // $('#rain').html(a.rain.3h);//unespected token
-        // $('#sunrise').html(a.sys.sunrise);
-        // $('#sunset').html(a.sys.sunset);
-        //$('#humi').html(a.main.humidity);
-        //$('#wind').html(a.wind.speed);
+        var dia = iconcode.endsWith("d")
+        // para teste das imagens
+        // iconcode = "50n";
+        if (dia == true) {
+          $("#default").toggleClass("day");
+          if (iconcode == "01d") {
+            $("#default").toggleClass("clear_sky");
+          }
+          if (iconcode == "02d") {
+            $("#default").toggleClass("few_clouds");
+          }
+          if (iconcode == "03d") {
+            $("#default").toggleClass("scattered_clouds");
+          }
+          if (iconcode == "04d") {
+            $("#default").toggleClass("broken_clouds");
+          }
+          if (iconcode == "09d") {
+            $("#default").toggleClass("shower_rain");
+          }
+          if (iconcode == "10d") {
+            $("#default").toggleClass("rain");
+          }
+          if (iconcode == "11d") {
+            $("#default").toggleClass("thunderstorm");
+          }
+          if (iconcode == "13d") {
+            $("#default").toggleClass("snow");
+          }
+          if (iconcode == "50d") {
+            $("#default").toggleClass("mist");
+          }
+          // for night
+          if (iconcode == "01n") {
+            $("#default").toggleClass("clear_sky_night");
+          }
+          if (iconcode == "02n") {
+            $("#default").toggleClass("few_clouds_night");
+          }
+          if (iconcode == "03n") {
+            $("#default").toggleClass("scattered_clouds_night");
+          }
+          if (iconcode == "04n") {
+            $("#default").toggleClass("broken_clouds_night");
+          }
+          if (iconcode == "09n") {
+            $("#default").toggleClass("shower_rain_night");
+          }
+          if (iconcode == "10n") {
+            $("#default").toggleClass("rain_night");
+          }
+          if (iconcode == "11n") {
+            $("#default").toggleClass("thunderstorm_night");
+          }
+          if (iconcode == "13n") {
+            $("#default").toggleClass("snow_night");
+          }
+          if (iconcode == "50n") {
+            $("#default").toggleClass("mist_night");
+          }
+        }
 
       });
     })
